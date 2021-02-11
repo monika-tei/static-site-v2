99cf75a19316abdecdfda08182e4c5940a2e9ced
Jonas Holbech <jh@jonasholbech.dk>
2021-02-11 13:05:51 +0100

    brands list done

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,9 @@
-const url = "https://kea-alt-del.dk/t7/api/products";
+const urlParams = new URLSearchParams(window.location.search);
+//in the URL grab id and store it's value in id
+const brandname = urlParams.get("brandname");
+console.log(brandname);
+document.querySelector("main>h2").textContent = brandname;
+const url = "https://kea-alt-del.dk/t7/api/products?brandname=" + brandname;
 
 fetch(url)
   .then(function (res) {
@@ -31,13 +36,14 @@ function showProduct(product) {
   //console.log(product);
   //soldOut onSale
   //grab the template
-  console.log(
-    "I am " + product.productdisplayname + "and i ahve id " + product.id
-  );
+
   const template = document.querySelector("#smallProductTemplate").content;
   //clone it
   const copy = template.cloneNode(true);
   //change content
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
   copy.querySelector(
     ".subtle"
   ).textContent = `${product.articletype} | ${product.brandname}`;
